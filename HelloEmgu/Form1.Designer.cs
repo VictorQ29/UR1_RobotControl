25967582ada11d16df3d03de14bc9fd4822c9510
VictorQ29 <98054563+VictorQ29@users.noreply.github.com>
2022-02-09 14:51:00 -0500

    Initial commit

---
+++ HelloEmgu/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace HelloEmgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emguPictureBox = new System.Windows.Forms.PictureBox();
+            this.binaryPictureBox = new System.Windows.Forms.PictureBox();
+            this.thresholdTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // emguPictureBox
+            // 
+            this.emguPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.emguPictureBox.Name = "emguPictureBox";
+            this.emguPictureBox.Size = new System.Drawing.Size(339, 315);
+            this.emguPictureBox.TabIndex = 0;
+            this.emguPictureBox.TabStop = false;
+            // 
+            // binaryPictureBox
+            // 
+            this.binaryPictureBox.Location = new System.Drawing.Point(460, 12);
+            this.binaryPictureBox.Name = "binaryPictureBox";
+            this.binaryPictureBox.Size = new System.Drawing.Size(328, 315);
+            this.binaryPictureBox.TabIndex = 1;
+            this.binaryPictureBox.TabStop = false;
+            // 
+            // thresholdTrackBar
+            // 
+            this.thresholdTrackBar.Location = new System.Drawing.Point(12, 356);
+            this.thresholdTrackBar.Maximum = 255;
+            this.thresholdTrackBar.Name = "thresholdTrackBar";
+            this.thresholdTrackBar.Size = new System.Drawing.Size(776, 56);
+            this.thresholdTrackBar.TabIndex = 2;
+            this.thresholdTrackBar.Scroll += new System.EventHandler(this.thresholdTrackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.thresholdTrackBar);
+            this.Controls.Add(this.binaryPictureBox);
+            this.Controls.Add(this.emguPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.emguPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binaryPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox emguPictureBox;
+        private System.Windows.Forms.PictureBox binaryPictureBox;
+        private System.Windows.Forms.TrackBar thresholdTrackBar;
+    }
+}
+
---
+++ HelloEmgu/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV;
+
+namespace HelloEmgu
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        private int _threshold = 150;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(0);
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+            thresholdTrackBar.Value = _threshold;
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+
+                int newHeight = (frame.Size.Height * emguPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(emguPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+                emguPictureBox.Image = frame.Bitmap;
+
+                emguPictureBox.Image = frame.Bitmap;
+
+                Mat grayFrame = new Mat();
+                Mat binaryFrame = new Mat();
+                CvInvoke.CvtColor(frame, grayFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.Threshold(grayFrame, binaryFrame, _threshold, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+
+                binaryPictureBox.Image = binaryFrame.Bitmap;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _captureThread.Abort();
+        }
+
+        private void thresholdTrackBar_Scroll(object sender, EventArgs e)
+        {
+            _threshold = thresholdTrackBar.Value;
+        }
+    }
+}
